ffbe25f2dd5da058616ef112609f3cfedf98e0cf
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-15 13:47:53 -0800

    created park model

--- app/models/park.py
+++ app/models/park.py
@@ -4,4 +4,7 @@ class Park(db.Model):
     __tablename__ = 'parks'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(40), nullable=False)
+    city = db.Column(db.String(40), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    imageURL = db.Column(db.String, nullable=False)
