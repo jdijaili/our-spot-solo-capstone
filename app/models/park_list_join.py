4218b01d2f61902c9bc47bc7fb4cc89c96080086
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-15 13:57:04 -0800

    created park_list_join model and edited list model

---
+++ app/models/park_list_join.py
@@ -0,0 +1,8 @@
+from .db import db
+
+class ParkListJoin(db.Model):
+    __tablename__ = 'park_list_joins'
+
+    id = db.Column(db.Integer, primary_key=True)
+    park_id = db.Column(db.Integer, nullable=False)
+    list_id = db.Column(db.Integer, nullable=False)
