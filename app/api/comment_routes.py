c2d74dbf578c8d373c36a5fee00c463cc3fae30c
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-16 21:03:06 -0800

    post comments route

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,6 +1,7 @@
 from crypt import methods
-from flask import Blueprint, jsonify
-from app.models import Comment
+from flask import Blueprint, jsonify, make_response, request
+from sqlalchemy import JSON
+from app.models import db, Comment
 
 comment_routes = Blueprint('comments', __name__)
 
@@ -11,4 +12,16 @@ def get_comments(park_id):
 
 @comment_routes.route('/<int:park_id>', methods=['POST'])
 def post_comment(park_id):
-    
+    comment = Comment(
+        park_id=park_id,
+        user_id=request.json['user_id'],
+        reply=request.json['reply'],
+        commentText=request.json['commentText']
+    );
+
+    if (comment):
+        db.session.add(comment)
+        db.session.commit()
+        return comment.to_JSON()
+    else:
+        return make_response({'errors': 'Error(s) on the comment occured'})
