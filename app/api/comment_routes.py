16d7967f7b7d451095899367f8e0fb945d87e122
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-16 19:34:55 -0800

    get comments route

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -3,4 +3,7 @@ from app.models import Comment
 
 comment_routes = Blueprint('comments', __name__)
 
-
+@comment_routes('/<int:park_id>')
+def get_comments(park_id):
+    comments = Comment.query.filter(Comment.id == park_id)
+    return jsonify([comment.to_JSON() for comment in comments])
