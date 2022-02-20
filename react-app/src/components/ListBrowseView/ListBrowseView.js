f037ef6b219ae20a9874d1c74437cda24e06d4cf
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-20 13:27:54 -0800

    list browse header css

--- react-app/src/components/ListBrowseView/ListBrowseView.js
+++ react-app/src/components/ListBrowseView/ListBrowseView.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom';
 import { getAllLists } from '../../store/lists';
 import './ListBrowseView.css'
@@ -7,14 +7,19 @@ import './ListBrowseView.css'
 const ListBrowseView = () => {
     const dispatch = useDispatch();
     const { userId } = useParams();
+    const lists = useSelector(state => state.lists)
 
     useEffect(() => {
-        dispatch(getAllLists(userId))
-    })
+        dispatch(getAllLists(userId));
+    }, [dispatch])
 
-    return(
-        <div>
-            <h1>List Browse View</h1>
+    return (
+        <div className='list-browse-page'>
+            <div className='header'>
+                <h1>List Browse View</h1>
+                <button className='new-list'>New List</button>
+            </div>
+
         </div>
     )
 }
