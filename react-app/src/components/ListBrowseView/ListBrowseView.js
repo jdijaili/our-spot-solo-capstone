58d1f6ddaebe3c81c93e1f5d722f0286efdda34d
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-20 13:45:29 -0800

    initial map for lists

--- react-app/src/components/ListBrowseView/ListBrowseView.js
+++ react-app/src/components/ListBrowseView/ListBrowseView.js
@@ -1,13 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getAllLists } from '../../store/lists';
 import './ListBrowseView.css'
 
 const ListBrowseView = () => {
     const dispatch = useDispatch();
     const { userId } = useParams();
-    const lists = useSelector(state => state.lists)
+    const lists = Object.values(useSelector(state => state.lists))
 
     useEffect(() => {
         dispatch(getAllLists(userId));
@@ -19,7 +19,13 @@ const ListBrowseView = () => {
                 <h1>List Browse View</h1>
                 <button className='new-list'>New List</button>
             </div>
-
+            <div>
+                {lists.map(list => (
+                    <Link to={`/lists/${list.id}`}>
+                        <h3>{list.title}</h3>
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
