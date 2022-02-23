91a11ebcfd51d790a5847cb6023e1ad7998243d4
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-22 18:41:32 -0800

    home page style

--- react-app/src/components/HomeView/HomeView.js
+++ react-app/src/components/HomeView/HomeView.js
@@ -1,28 +1,39 @@
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import './HomeView.css'
 
 const HomeView = () => {
-    const user = useSelector(state => state.session.user)
+    const user = useSelector(state => state.session.user);
 
     const logged_in = (
         <div className='home'>
             <div className='home-content'>
                 <h1>Welcome to Our Spot</h1>
-                <p>caught a vibe</p>
-                <p>baby are you coming for the ride</p>
+                <p>caught a vibe, baby are you coming for the ride</p>
+                <Link to='/parks'>
+                    <button className='home-explore-button'>Explore Parks</button>
+                </Link>
+            </div>
+            <div>
+                <img className='home-image' src='https://res.cloudinary.com/jenn/image/upload/v1645572949/our-spot/Untitled_Artwork_3_myamif.png' alt='park' />
             </div>
         </div>
     )
 
     const splash = (
-        <>
-            <div className='home'>
-                <h1>Sign up or Log in to explore parks in your area!</h1>
+        <div className='home'>
+            <div className='home-content'>
+                <h1>Welcome to Our Spot</h1>
+                <p>Sign in or start exploring local parks!</p>
+                <button className='home-explore-button'>Explore Parks</button>
             </div>
-        </>
+            <div>
+                <img className='home-image' src='https://res.cloudinary.com/jenn/image/upload/v1645572949/our-spot/Untitled_Artwork_3_myamif.png' alt='park' />
+            </div>
+        </div>
     )
 
-    return(
+    return (
         <div>
             {user ? logged_in : splash}
         </div>
--- react-app/src/components/PageNotFound/PageNotFound.js
+++ react-app/src/components/PageNotFound/PageNotFound.js
@@ -1,8 +1,9 @@
 const PageNotFound = () => {
     return(
-        <div>
+        <div className='404-page'>
             <h1>Page Not Found</h1>
             <iframe
+            className='404-music-vid'
             width="560"
             height="315"
             src="https://www.youtube.com/embed/2dTMIH5gCHg"
@@ -10,6 +11,9 @@ const PageNotFound = () => {
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen></iframe>
+            <div className=''>
+                <img className='home-image' src='https://res.cloudinary.com/jenn/image/upload/v1645572949/our-spot/Untitled_Artwork_3_myamif.png' alt='park' />
+            </div>
         </div>
     )
 };
