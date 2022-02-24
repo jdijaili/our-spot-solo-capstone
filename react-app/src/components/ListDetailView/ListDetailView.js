bedb63c91d509ea968dafb9175db10a7b0785a25
jdijaili <jennifer.dijailirx@gmail.com>
2022-02-23 20:02:18 -0800

    styled list detail view delete button

--- react-app/src/components/ListDetailView/ListDetailView.js
+++ react-app/src/components/ListDetailView/ListDetailView.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useHistory } from 'react-router-dom';
+import { useParams, useHistory, Link } from 'react-router-dom';
 import { deleteList, editList, getList } from '../../store/lists';
 import { deleteListParkRef, getParksForList } from '../../store/parks';
 import ParkCard from '../ParkCard/ParkCard';
@@ -90,11 +90,35 @@ const ListDetailView = () => {
         if (showForm === false) {
             setShowForm(true);
         } else if (showForm === true) {
-            setTitle('');
-            setDescription('');
+            setTitle(list[0]?.title);
+            setDescription(list[0]?.description);
             setShowForm(false);
         }
     }
+
+    const parksMap = (
+        <div className='parks-browse-cards'>
+            {parks.map(park => (
+                <div key={park.id} className='park-card-individual'>
+                    <div className='park-delete-button-container'>
+                        <div>
+                            <button className='park-delete-button' value={park.id} onClick={handleParkDelete}>X</button>
+                        </div>
+                    </div>
+                    <ParkCard park={park} className='park-card-comp'/>
+                </div>
+            ))}
+        </div>
+    )
+
+    const noParks = (
+        <div className='no-parks-block'>
+            <h2>There are no parks to display</h2>
+            <Link to='/parks'>
+                <button className='explore-parks-button'>Explore Parks</button>
+            </Link>
+        </div>
+    )
 
     return (
         <div className='list-detail-page'>
@@ -134,14 +158,7 @@ const ListDetailView = () => {
                     </div>
                 }
             </div>
-            <div className='parks-browse-cards'>
-                {parks.map(park => (
-                    <div key={park.id}>
-                        <ParkCard park={park} />
-                        <button value={park.id} onClick={handleParkDelete}>Delete</button>
-                    </div>
-                ))}
-            </div>
+            {parks.length ? parksMap : noParks}
         </div>
     )
 };
