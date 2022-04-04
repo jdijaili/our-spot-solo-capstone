a3aba4973716d4659cd2a17db0d5304a85d71d1d
jdijaili <jennifer.dijailirx@gmail.com>
2022-04-04 10:56:52 -0700

    changed alta plaza picture from heic to jpg

--- app/seeds/parks.py
+++ app/seeds/parks.py
@@ -14,7 +14,7 @@ def seed_parks():
         city='San Francisco',
         state='California',
         description='Alta Plaza is known for its panoramic view of the city. There\'s a playground, tennis court, and grand staircase that rises to a central plateau to admire the view from.',
-        imageURL='https://res.cloudinary.com/jenn/image/upload/v1649094389/our-spot/IMG_4025_w9sich.heic'
+        imageURL='https://res.cloudinary.com/jenn/image/upload/v1649094917/our-spot/IMG_4025_eeplih.jpg'
     )
 
     alamo_square = Park(
